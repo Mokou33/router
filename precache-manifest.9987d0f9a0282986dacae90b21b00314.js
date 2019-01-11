42779d40182fa074a1c0eeb8830cce1baa389cd0
scarlet-moon <978542146@qq.com>
2019-01-11 11:38:52 +0800

    第一次添加

---
+++ precache-manifest.9987d0f9a0282986dacae90b21b00314.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4de5dd521ee864cc584c8f2880c350d2",
+    "url": "./static/media/logo.4de5dd52.png"
+  },
+  {
+    "revision": "a15965282f887b9884d4dc836101cc7f",
+    "url": "./static/media/iconfont.a1596528.ttf"
+  },
+  {
+    "revision": "5bb870bc65e06fbe4d138e080422803c",
+    "url": "./static/media/iconfont.5bb870bc.eot"
+  },
+  {
+    "revision": "05165552e3fd6ea332ddec30b816e57d",
+    "url": "./static/media/iconfont.05165552.svg"
+  },
+  {
+    "revision": "489a19fb0dae5afef1f9c475972383d6",
+    "url": "./static/media/404.489a19fb.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "21454bd043289bb860d4",
+    "url": "./static/js/main.21454bd0.chunk.js"
+  },
+  {
+    "revision": "759fc3949fd5539bddfa",
+    "url": "./static/js/1.759fc394.chunk.js"
+  },
+  {
+    "revision": "21454bd043289bb860d4",
+    "url": "./static/css/main.2f6aed60.chunk.css"
+  },
+  {
+    "revision": "7abd4fa1bb937da550b4f8e2b10d90f4",
+    "url": "./index.html"
+  }
+];
